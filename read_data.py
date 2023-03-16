80128799e2a8f1fa28c4c438ff606a9d5ed9b72c
No-drink <2019302070169@whu.edu.cn>
2023-03-16 17:50:54 +0800

    original

---
+++ read_data.py
@@ -0,0 +1,57 @@
+﻿# lat: 纬度
+# lon: 经度
+# TEM: 气温
+# PRE_1h: 1小时降水量
+# RHU: 相对湿度
+# WD: 风向
+# WS: 风速
+# SR: 日照时数
+# TCC: 总云量
+import os
+import re
+import netCDF4
+import pandas as pd
+
+folder_path = "./data/given_data" # Change this to your folder path
+
+# Analysis the data information
+def datainform(file_name):
+    dtm = re.findall(r"km(.+?).nc", file_name)[0]
+    period, starttime, endtime = re.findall(r"(\d+)", dtm)
+    starttime = pd.to_datetime(starttime)
+    endtime = pd.to_datetime(endtime)
+    return [starttime,endtime]
+
+def read_all(folder_path, df):
+    file_list = os.listdir(folder_path)
+    for file in file_list:
+        file2read = netCDF4.Dataset(f"{folder_path}/{file}", "r") # Open the file in read mode
+        endtime = datainform(file)[1]
+        datas_in = []
+        for TEM in file2read.variables['TEM'][:].data:
+            datas_in.extend(TEM)
+        for PRE_1h in file2read.variables['PRE_1h'][:].data:
+            datas_in.extend(PRE_1h)
+        for RHU in file2read.variables['RHU'][:].data:
+            datas_in.extend(RHU)
+        for WD in file2read.variables['WD'][:].data:
+            datas_in.extend(WD)
+        for WS in file2read.variables['WS'][:].data:
+            datas_in.extend(WS)
+        for SR in file2read.variables['SR'][:].data:
+            datas_in.extend(SR)
+        for TCC in file2read.variables['TCC'][:].data:
+            datas_in.extend(TCC)
+        df.loc[:,endtime] = datas_in
+    return True
+
+# Generate the index
+file_example = netCDF4.Dataset(f"./data/given_data/JTBZDB1D_yb_gdfs-meter_3d2.5km15min_202111301200_202112010800.nc", "r") # Open the file in read mode
+lats = file_example.variables['lat'][:].data
+lons = file_example.variables['lon'][:].data
+categories = ['TEM','PRE_1h','RHU','WD','WS','SR','TCC']
+index = pd.MultiIndex.from_product([categories, lats, lons], names=['category', 'lat', 'lon'])
+weather_data = pd.DataFrame(index=index)
+
+if (read_all(folder_path, weather_data)):
+    weather_data.to_csv('weather_data.csv', index=True, header=True)
---
+++ spatial_interpolation.py
@@ -0,0 +1,24 @@
+﻿# 导入pykrige模块
+import pykrige as pk
+import numpy as np
+
+# 假设你有一些2D数据，例如温度或高度
+x = [1.0, 2.0, 3.0, 4.0] # x坐标
+y = [1.0, 2.0, 3.0, 4.0] # y坐标
+z = [10.0, 20.0, 30.0, 40.0] # 数据值
+
+# 创建一个普通克里金对象
+OK = pk.OrdinaryKriging(x,y,z)
+
+# 指定一个网格来进行插值，例如5x5的网格
+gridx = np.linspace(1.0,4.0,num=5)
+gridy = np.linspace(1.0,4.0,num=5)
+
+# 调用execute方法来进行插值，并得到插值结果和方差估计
+z_ok,var_ok = OK.execute('grid',gridx,gridy)
+
+# 创建一个通用克里金对象，指定趋势模型为线性（ax+by+c）
+UK = pk.UniversalKriging(x,y,z,trend='linear')
+
+# 调用execute方法来进行插值，并得到插值结果和方差估计
+z_uk,var_uk = UK.execute('grid',gridx,gridy)
